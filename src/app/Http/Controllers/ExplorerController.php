c8a2ef34af4e212d3c8b63411ef0c500c4a3073c
Eduardo-Batist4 <carloswe3st@gmail.com>
2025-03-15 17:33:47 -0300

    controller and routes for the explorer completed

--- src/app/Http/Controllers/ExplorerController.php
+++ src/app/Http/Controllers/ExplorerController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Explorer;
 use Illuminate\Http\Request;
 
 class ExplorerController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $explorers = Explorer::all();
+
+        return response($explorers, 200);
+    }
+
+    public function store(Request $req)
+    {
+        $validateData = $req->validate([
+            'name' => 'required|min:4|max:50',
+            'age' => 'required|integer|min:18|max:100',
+            'latitude' => 'nullable|numeric|between:-90,90',
+            'longitude' => 'nullable|numeric|between:-180,180'
+        ]);
+
+        $explorer = Explorer::create($validateData);
+
+        return response()->json([
+            'message' => 'Explorer created successfully!',
+            'explorer' => $explorer
+        ], 201);
+    }
 }
